edbbbb209f944d26813bd9393e4afb771639ce68
theo82 <theoaristi53@gmail.com>
2025-12-27 17:35:49 +0200

    building the layout

---
+++ src/App.js
@@ -0,0 +1,32 @@
+export default function App() {
+  return (
+    <div className="app">
+      <Logo />
+      <Form />
+      <PackingList />
+      <Stats />
+    </div>
+  )
+}
+
+function Logo() {
+  return <h1> 🏝 Far Away 🧳 </h1>
+}
+
+function Form() {
+  return <div className="add-form">
+    <h3>What do you need for your first trip?</h3>
+  </div>
+}
+
+function PackingList() {
+  return <div className="list">LIST</div>
+}
+
+function Stats() {
+  return (
+  <footer className="stats">
+    <em>🧳 You have X items on your lit, and you already packed X (X%)</em>
+  </footer>
+  )
+}
