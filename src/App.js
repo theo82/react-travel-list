ecc5e0b1663c2f8145280fe85dc02fc6e9625a92
theo82 <theoaristi53@gmail.com>
2026-01-15 18:48:03 +0200

    Sorting Items

--- src/App.js
+++ src/App.js
@@ -88,10 +88,26 @@ function Form({ onAddItems }) {
 }
 
 function PackingList({ items, onDeleteItem, onToggleItem }) {
+  const [sortBy, setSortBy] = useState('input')
+
+  let sortedItems
+
+  if (sortBy === 'input') sortedItems = items
+
+  if (sortBy === 'description')
+    sortedItems = items
+      .slice()
+      .sort((a, b) => a.description.localeCompare(b.description))
+
+  if (sortBy === 'packed')
+    sortedItems = items
+      .slice()
+      .sort((a, b) => Number(a.packed) - Number(b.packed))
+
   return (
     <div className='list'>
       <ul>
-        {items.map((item) => (
+        {sortedItems.map((item) => (
           <Item
             item={item}
             onDeleteItem={onDeleteItem}
@@ -100,6 +116,14 @@ function PackingList({ items, onDeleteItem, onToggleItem }) {
           />
         ))}
       </ul>
+
+      <div className='actions'>
+        <select value={sortBy} onChange={(e) => setSortBy(e.target.value)}>
+          <option value='input'>sort by input order</option>
+          <option value='description'>Sort by description</option>
+          <option value='packed'>Sort by packed status</option>
+        </select>
+      </div>
     </div>
   )
 }
@@ -119,6 +143,7 @@ function Item({ item, onDeleteItem, onToggleItem }) {
     </li>
   )
 }
+
 function Stats({ items }) {
   if (!items.length)
     return <p className='stats'>Start adding some items in your list </p>
